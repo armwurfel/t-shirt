d35e696b2611a187188c099888e4d1dd0a33342a
Abdur Rehman Majeed <arm.wurfel@gmail.com>
2019-11-05 16:52:15 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/t-shirt/precache-manifest.f78b68ecf74118073e4aea1905e1cafa.js"
+  "/t-shirt/precache-manifest.18eacb3fa4d4aebe32167313fc4477a8.js"
 );
 
 self.addEventListener('message', (event) => {

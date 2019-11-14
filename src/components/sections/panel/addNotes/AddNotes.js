a5261dae4c46805a92f8581100560ada7845d60a
armwurfel <arm.wurfel@gmail.com>
2019-11-14 23:29:07 +0500

    Add Note Updated

--- src/components/sections/panel/addNotes/AddNotes.js
+++ src/components/sections/panel/addNotes/AddNotes.js
@@ -7,7 +7,24 @@ class AddNotes extends Component {
     render() {
         return (
             <div className="card">
-                 <CardHeader title="Add Notes" />
+                 <CardHeader title="Notes" />
+                 <form class="form notes">
+                     <div class="notes-heading">Leave a note for our production team.</div>
+                     <div class="notes-subheading">Your note will be seen when we receive your order.</div>
+                     <textarea class="notes-textarea" cols="4" maxlength="4000" name="notes" placeholder="Your note" rows="4" tabindex="1"></textarea>
+                     <div class="notes-tools">
+                         <button class="cta-primary notes-submit" tabindex="2" type="submit">Save Note</button>
+                    </div>
+                    <div class="notes-instructions">
+                        <div class="notes-instructionsHeader">Use notes to request:</div>
+                        <div class="notes-instructionsBody">
+                            <div>Special artwork instructions</div>
+                            <div>Pantone color matching codes (PMS)</div>
+                            <div>Art rotations and touch-ups</div>
+                            <div>Sleeve printing</div>
+                        </div>
+                    </div>
+                </form>
             </div>
         )
     }
